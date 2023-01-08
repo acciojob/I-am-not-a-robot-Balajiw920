aa49994e86adda1f9e74f15cee70d1c6a266350a
Balajiw920 <wbalaji2021@gmail.com>
2023-01-08 21:02:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+const images = document.querySelectorAll('img');
+
+images.forEach(image => {
+  image.addEventListener('click', () => {
+    image.classList.toggle('selected');
+  });
+});
+const resetButton = document.getElementById('reset');
+
+resetButton.addEventListener('click', () => {
+  images.forEach(image => {
+    image.classList.remove('selected');
+  });
+});
+const verifyButton = document.getElementById('verify');
+const para = document.getElementById('para');
+
+verifyButton.addEventListener('click', () => {
+  const selectedImages = document.querySelectorAll('.selected');
+  if (selectedImages[0].class
