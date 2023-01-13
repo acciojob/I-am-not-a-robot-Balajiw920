d6486695082fc5e12aa6ebffb446df66becc9bf8
Balajiw920 <wbalaji2021@gmail.com>
2023-01-13 21:32:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,55 @@
-const images = [...]; // array of image URLs
-let selectedImageClasses = []; // array to store the class names of the selected images
+//your code here
+let n = Math.floor(Math.random() * 5 + 1);
+let main = document.getElementsByTagName('main')[0];
+let images = document.querySelectorAll('img');
+var randomImage = document.getElementsByClassName('none')[0];
+let imageContainer = document.getElementsByClassName("image")[0];
+let arr = [];
+let reset = document.getElementById('reset');
+reset.style.display = 'none';
+let verify = document.getElementById('verify');
+let result = document.getElementsByClassName('para')[0];
+verify.style.display = 'none';
+randomImage.setAttribute("class", `img${n}`);
+images.forEach((e)=>{
+  e.addEventListener('click', ()=>{
+    e.style.border = "8px solid blue";
+    arr.push(e.getAttribute('class'));
+    if(arr.length>0 && arr.length<3){
+      reset.style.display = 'block';
+    }
+    if(arr.length==2){
+      verify.style.display = 'block';
+    }
+    if(arr.length>2){
+      reset.style.display = 'none';
+      verify.style.display = 'none';
+      let para = document.createElement('p');
+      para.innerText = "Please select only 2 images. Kindly refresh the page";
+      main.append(para);
+    }
+  })
+})
 
-function renderImages() {
-  const imageContainer = document.getElementById('image-container');
-  // shuffle the images array
-  shuffle(images);
-
-  // render the images and give them the appropriate class names
-  for (let i = 0; i < images.length; i++) {
-    const img = document.createElement('img');
-    img.src = images[i];
-    img.className = `img${i + 1}`;
-    img.addEventListener('click', handleImageClick);
-    imageContainer.appendChild(img);
+reset.addEventListener('click',()=>{
+  for(let i = 0;i<=arr.length;i++){
+    arr.pop();
   }
-}
+  reset.style.display = "none";
+  result.innerText = "";
+  verify.style.display = 'none'
+  images.forEach((e)=>{
+      e.style.border = 'none';
+  })
+})
 
-function handleImageClick(event) {
-  const clickedImageClass = event.target.className;
-  selectedImageClasses.push(clickedImageClass);
-
-  // if two images have been selected, show the verify button
-  if (selectedImageClasses.length === 2) {
-    const verifyButton = document.createElement('button');
-    verifyButton.innerHTML = 'Verify';
-    verifyButton.id = 'verify';
-    verifyButton.addEventListener('click', handleVerifyClick);
-    document.body.appendChild(verifyButton);
+verify.addEventListener('click',()=>{
+  if(arr[0]==arr[1]){
+     result.innerText = 'You are a human. Congratulations!.';
+  }else{
+    result.innerText = "We can't verify you as a human. You selected the non-identical tiles.";
   }
+})
 
-  // show the reset button
-  const resetButton = document.createElement('button');
-  resetButton.innerHTML = 'Reset';
-  resetButton.id = 'reset';
-  resetButton.addEventListener('click', handleResetClick);
-  document.body.appendChild(resetButton);
-}
-
-function handleVerifyClick() {
-  const para = document.createElement('p');
-  para.id = 'para';
-  if (selectedImageClasses[0] === selectedImageClasses[1]) {
-    para.innerHTML = 'You are a human. Congratulations!';
-  } else {
-    para.innerHTML = "We can't verify you as a human. You selected the non-identical tiles.";
-  }
-  document.body.appendChild(para);
-
-  // hide the verify button
-  document.getElementById('verify').style.display = 'none';
-}
-
-function handleResetClick() {
-  // remove the images and the reset button
-  const imageContainer = document.getElementById('image-container');
-  while (imageContainer.firstChild) {
-    imageContainer.removeChild(imageContainer.firstChild);
-  }
-  document.getElementById('reset').style.display = 'none';
-
-  // reset the selectedImageClasses array
-  selectedImageClasses = [];
-
-  // render the images again
-  renderImages();
-}
-
-function shuffle(array) {
-  // shuffle the array using the Fisher-Yates shuffle algorithm
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-
-renderImages();
+imageContainer.append(randomImage);
+// random image was added till yet
